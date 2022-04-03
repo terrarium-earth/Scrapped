5dba14e261c3a160c28e0e59c22f9f3626b909da
abused_master <techperson71@gmail.com>
2022-04-03 19:22:45 -0400

    Update PinkSlimeFluidBlock.java

--- src/main/java/dev/onyxstudios/minefactoryrenewed/block/fluid/PinkSlimeFluidBlock.java
+++ src/main/java/dev/onyxstudios/minefactoryrenewed/block/fluid/PinkSlimeFluidBlock.java
@@ -3,7 +3,6 @@ package dev.onyxstudios.minefactoryrenewed.block.fluid;
 import dev.onyxstudios.minefactoryrenewed.entity.PinkSlimeEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FlowingFluid;
