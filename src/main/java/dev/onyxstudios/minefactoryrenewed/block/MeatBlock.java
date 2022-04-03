1080e2747c00e72cd4ce8dcbe0df8425c0e9d40e
abused_master <techperson71@gmail.com>
2022-04-03 12:13:29 -0400

    Update MeatBlock.java

--- src/main/java/dev/onyxstudios/minefactoryrenewed/block/MeatBlock.java
+++ src/main/java/dev/onyxstudios/minefactoryrenewed/block/MeatBlock.java
@@ -1,12 +1,16 @@
 package dev.onyxstudios.minefactoryrenewed.block;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 
 public class MeatBlock extends Block {
 
     public MeatBlock() {
-        super(BlockBehaviour.Properties.of(Material.STONE).strength(0.55f));
+        super(BlockBehaviour.Properties.of(Material.CLAY)
+                .strength(0.55f)
+                .sound(SoundType.SLIME_BLOCK)
+        );
     }
 }
