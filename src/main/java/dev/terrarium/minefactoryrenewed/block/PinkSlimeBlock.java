81da575175de7136aa3be2f42521658f931c4941
abused_master <techperson71@gmail.com>
2022-05-26 15:54:28 -0400

    OPPPPPPPP Pink Slime

--- src/main/java/dev/terrarium/minefactoryrenewed/block/PinkSlimeBlock.java
+++ src/main/java/dev/terrarium/minefactoryrenewed/block/PinkSlimeBlock.java
@@ -1,11 +1,15 @@
 package dev.terrarium.minefactoryrenewed.block;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.SlimeBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -18,6 +22,24 @@ public class PinkSlimeBlock extends SlimeBlock {
                 .sound(SoundType.SLIME_BLOCK)
                 .noOcclusion()
         );
+    }
+
+    @Override
+    public void updateEntityAfterFallOn(BlockGetter level, Entity entity) {
+        if (entity.isSuppressingBounce()) {
+            super.updateEntityAfterFallOn(level, entity);
+        } else {
+            this.bounceUp(entity);
+        }
+
+    }
+
+    private void bounceUp(Entity entity) {
+        Vec3 vec3 = entity.getDeltaMovement();
+        if (vec3.y < 0.0D) {
+            double d0 = entity instanceof LivingEntity ? 2.2D : 1.6D;
+            entity.setDeltaMovement(vec3.x, -vec3.y * d0, vec3.z);
+        }
     }
 
     @NotNull
